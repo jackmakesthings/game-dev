2bd9a136aae9804426588949b0148f9e3930b64b
Jack Gold <jgold@hugeinc.com>
2015-10-06 17:33:44 -0400

    experimenting with serialization, trying to see what inst2dict / dict2inst can do

---
+++ return-false/tutorial-draft/sandbox/class_Person.gd
@@ -0,0 +1,44 @@
+extends Node
+
+var name
+var age
+var height
+var weight
+var job
+var has_pets
+var city
+var interests
+
+func parse_height(ht):
+	var feet = floor( ht / 12 )
+	var inches = ht - (feet*12)
+	return "" + feet + "' " + inches
+
+func _setup():
+	var rtl = RichTextLabel.new()
+	rtl.set_size(Vector2(600,400))
+	add_child(rtl)
+	
+	#if( name != "" ):
+	#	rtl.add_text(name)
+	#	rtl.newline()
+	
+	if( age > 0 ):
+		rtl.add_text(str(age))
+		rtl.newline()
+		
+	if( height > 0 and weight > 0 ):
+
+		rtl.add_text(parse_height(height))
+		rtl.add_text(' / ')
+		rtl.add_text(weight + " lbs")
+		rtl.newline()
+		
+	if( has_pets ):
+		rtl.add_text("Pets: YES")
+	else:
+		rtl.add_text("Pets: NO")
+		rtl.newline()
+
+func setup():
+	print(self.get_property_list())	
---
+++ return-false/tutorial-draft/sandbox/definition_template.gd
@@ -0,0 +1,10 @@
+extends Node
+
+export(String) var name
+export(int) var age
+export(float) var height
+export(float) var weight
+export(String) var job
+export(bool) var has_pets
+export(String) var city
+export(Array) var interests
