e5eaa2cce94c994889230521fea96fad8e134020
Jack Gold <jgold@hugeinc.com>
2015-12-16 11:33:42 -0500

    closes #28 - prototyped "email terminal" interface -- very similar to journal but a bit more flexible, might retrofit journal to this later

--- return-false/tutorial-draft/active-partials/interface/entry_container.gd
+++ return-false/tutorial-draft/active-partials/interface/entry_container.gd
@@ -1,7 +1,7 @@
 # entry_container.gd
 # like the journal ui but a bit more generic
 # planned for use with "email terminals" etc
-extends Control
+extends CanvasLayer
 
 const Entry = preload("entry.gd")
 
@@ -13,8 +13,10 @@ var active_entry
 var is_open
 
 var entry_list_box
+var saved_entry_list_box
 var content_box
 var dialog
+var status_bar
 
 
 func make_entry(args):
@@ -28,7 +30,10 @@ func add_entry(entry, set=local_entries):
 	entry_button.set_text(entry.entry_label)
 	entry_button.set_meta("id", entry.entry_id)
 	entry_button.set_text_align(0)
-	entry_list_box.add_child(entry_button)
+	if( set == local_entries ):
+		entry_list_box.add_child(entry_button)
+	else:
+		saved_entry_list_box.add_child(entry_button)
 	entry_button.connect("pressed", self, "show_entry", [entry.entry_id])
 
 
@@ -49,47 +54,53 @@ func show_entry(entry_id):
 	content_box.set_bbcode(active_entry.entry_content)
 	
 func show_next_entry():
-	pass
-#	var current = entry_list_box.get_parent().get_pressed_button().get_position_in_parent()
-#	var next = entry_list_box.get_child(1 + current)
-#	if( next == null ):
-#		return
-#	elif( next extends HSeparator ):
-#		next = entry_list_box.get_child(2 + current)
-#	elif( next extends Button ):
-#		entry_list_box.get_parent().set_pressed_button(next)
-#		show_entry(next.get_meta("id"))
+	var current = entry_list_box.get_pressed_button().get_position_in_parent()
+	var next = entry_list_box.get_child(1 + current)
+	if( next == null ):
+		return
+	elif( next extends HSeparator ):
+		next = entry_list_box.get_child(2 + current)
+	elif( next extends Button ):
+		next = next
+		
+	entry_list_box.set_pressed_button(next)
+	show_entry(next.get_meta("id"))
 	
 func show_prev_entry():
-	pass
-#	var current = entry_list_box.get_parent().get_pressed_button().get_position_in_parent()
-#	var prev = entry_list_box.get_child(current-1)
-#	if( prev == null ):
-#		return
-#	elif( prev extends HSeparator ):
-#		prev = entry_list_box.get_child(current-2)
-#	elif( prev extends Button ):
-#		entry_list_box.get_parent().set_pressed_button(prev)
-#		show_entry(prev.get_meta("id"))
+	var current = entry_list_box.get_pressed_button().get_position_in_parent()
+	var prev = entry_list_box.get_child(current-1)
+	if( prev == null or prev extends HSeparator or prev.get_name() == "entry_header"):
+		return
+	entry_list_box.set_pressed_button(prev)
+	show_entry(prev.get_meta("id"))
 
 
-func save_entry(entry_id):
-	pass
+func save_entry():
+	var entry = get_entry_by(active_entry.entry_id)
+	add_entry(active_entry, saved_entries)
+	status_bar.set_text("Saved entry '"+ entry.entry_label + "' to local storage")
+	
 	
 func open():
+	set_layer(5)
 	dialog.popup_centered()
-	entry_list_box.get_parent().set_pressed_button(entry_list_box.get_child(0))
+	entry_list_box.set_pressed_button(entry_list_box.get_child(0))
 	is_open = true
 
 func close():
 	dialog.hide()
+	set_layer(-1)
 	is_open = false
-
+	queue_free()
 
 func set_internals():
 	content_box = find_node("entry_text")
 	dialog = find_node("dialog")
-	entry_list_box = find_node("entries")
+	entry_list_box = find_node("entry_list")
+	saved_entry_list_box = find_node("saved_entry_list")
+	status_bar = find_node("status")
+	
+	
 
 func _ready():
 	set_internals()
