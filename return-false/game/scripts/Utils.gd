cef0d69fd3a1debd636d01215eb78f7d24e5ab94
Jack Gold <jgold@hugeinc.com>
2016-04-13 16:21:18 -0400

    autoloads need to inherit from Node to work

--- return-false/game/scripts/Utils.gd
+++ return-false/game/scripts/Utils.gd
@@ -1,4 +1,4 @@
-# Utility & shared methods (autoloaded as Utils)
-
+# Utility & shared methods (autoloaded as utils)
+extends Node
 func is_click(ev):
 	return (ev.type == InputEvent.MOUSE_BUTTON and ev.pressed and ev.button_index == 1)
