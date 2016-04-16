ef82f16a064ddcb33ceeb4a21af912e0d019449e
Jack Gold <jgold@hugeinc.com>
2016-04-16 19:08:23 -0400

    closes #68 - generic, super-minimal test of the config file functions. lives in the Utils autoload for now.

--- return-false/game/scripts/Utils.gd
+++ return-false/game/scripts/Utils.gd
@@ -1,5 +1,41 @@
 # Utility & shared methods (autoloaded as utils)
 extends Node
 
+
+
 func is_click(ev):
 	return (ev.type == InputEvent.MOUSE_BUTTON and ev.pressed and ev.button_index == 1)
+
+
+
+# Save/load config data
+var config_path = "user://config.cfg"
+
+
+func save_cfg(data):
+	var f = ConfigFile.new()
+	f.load(config_path)
+	for s in data:
+		f.set_value(s.section, s.key, s.val)
+	f.save(config_path)
+	
+
+func load_cfg():
+	var f = ConfigFile.new()
+	f.load(config_path)
+	return f
+
+
+# Testing for the config functions
+func test_cfg():
+	save_cfg([
+		{
+			section = "Character",
+			key = "pronouns",
+			val = "she/her"
+		}
+	])
+
+	var data = load_cfg()
+	var pronouns = data.get_value("Character", "pronouns")
+	print("You chose to use ", pronouns, ".")
