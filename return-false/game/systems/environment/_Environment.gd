cc0dab165937d954b739aff55bae390c08fe5d67
Jack Gold <hello@jackmakesthings.com>
2016-05-17 17:47:45 -0400

    closes #81 - still seeing a stack frame on this line in the debugger (whyyy) but there's no longer an error attached to it

--- return-false/game/systems/environment/_Environment.gd
+++ return-false/game/systems/environment/_Environment.gd
@@ -56,7 +56,7 @@ func _unhandled_input(ev):
 		var end = get_viewport_transform().affine_inverse().xform(ev.global_pos)
 		
 		# Flag used mostly for placement of the outline
-		var adjust_path = false
+		var adjust_path = true
 		
 		# Find the closest tile center to where we clicked
 		var tilepos = tiles.world_to_map(end)
@@ -67,8 +67,10 @@ func _unhandled_input(ev):
 		# See what kind of tile we're trying to walk to
 		# If it's unwalkable, the actor will go to the next closest point on the way.
 		var tile_at = tiles.get_cell(tilepos.x, tilepos.y)
-		if !tiles.get_tileset().tile_get_navigation_polygon(tile_at):
+		if tile_at == -1:
 			adjust_path = true
+		elif tiles.get_tileset().tile_get_navigation_polygon(tile_at):
+			adjust_path = false
 
 		emit_signal("walk_to", nav, end, adjust_path, tiles)
 		
