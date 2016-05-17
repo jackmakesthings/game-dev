24937e9da603addd43407f9024777a4fcb6514ed
Jack Gold <hello@jackmakesthings.com>
2016-05-17 18:00:42 -0400

    documentation: inline docs for main.gd

--- return-false/game/main.gd
+++ return-false/game/main.gd
@@ -1,12 +1,34 @@
 # Main game logic
+# This lives on the root Game node and holds global setup methods.
 
 extends Node
+
+# Member vars
+
+# Scene        : Node; reference to the current environment
+# Player       : Node; reference to the Player character tree
+# MessageUI    : Node; reference to the Dialgoue UI 
+# NPCManager   : Node; reference to the handler for all NPCs
+# QuestManager : Node; reference to the handler for quests
+
 var Scene = null
 var Player = null
 var MessageUI = null
 var NPCManager = null
 var QuestManager = null
 
+
+# Methods
+
+
+##
+# setup(name, path, raise)
+# Generic helper for initializing singletons and global reference nodes
+#
+# @name : String; the name to instance and reference the scene as
+# @path : String; path to the scene file
+# @raise : Boolean; whether to move the instance once it's created
+##
 func setup(name, path, raise=false):
 	var _instance = load(path).instance()
 	add_child(_instance)
@@ -15,7 +37,14 @@ func setup(name, path, raise=false):
 	if raise:
 		_instance.raise()
 	return self[name]
-	
+
+
+##
+# set_scene(scene_path)
+# Like setup, but specialized for setting the initial environment scene
+#
+# @scene_path : String; path to the environment scene file
+##
 func set_scene(scene_path):
 	var _scene = load(scene_path).instance()
 	if Scene != null:
@@ -25,6 +54,24 @@ func set_scene(scene_path):
 	Scene = _scene
 	return Scene
 
+
+##
+# change_scene(scene_path)
+# Helper for environments that also takes care of putting the Player
+# where it needs to go (as a child of Scene.object_layer)
+#
+# @scene_path : String; path to the new environment scene file
+##
+func change_scene(scene_path):
+	Player = null
+	set_scene(scene_path)
+	set_player()
+
+
+##
+# set_player
+# Another specialized setup method, this one for the Player node
+##
 func set_player():
 	var _player = load("res://systems/character/Player.tscn").instance()
 	var _destination = Scene.object_layer
@@ -34,14 +81,23 @@ func set_player():
 	Scene.connect('walk_to', Player, 'update_path')
 	return Player
 
-func _ready():
-	set_scene("res://systems/ui/Splash.tscn")
-	
+
+##
+# _enter_game
+# This gets called when switching from a non-game scene 
+# (like the initial game menu) to an in-game one.	
+##	
 func _enter_game():	
 	setup('MessageUI', "res://systems/dialogue/Dialogue.Example.tscn")
-	set_scene("res://systems/environment/_Environment.tscn")
-	set_player()
+	change_scene("res://systems/environment/_Environment.tscn")
 	setup('NPCManager', "res://systems/npc/NPC.Manager.tscn")
 	setup('QuestManager', "res://systems/quests/Quest.Manager.tscn", true)
 	MessageUI.raise()
 
+
+##
+# _ready
+# Starts the game at the base game menu scene
+##
+func _ready():
+	set_scene("res://systems/ui/Splash.tscn")
