aaec7ce0621da66cfca7ef68f3413aa9327832ee
Jack Gold <hello@jackmakesthings.com>
2016-08-14 11:22:26 -0400

    bitgrid refactor continues; is looking good on the front-end but the code is starting to get tangly again. pausing for now.

--- return-false/game/systems/puzzles/bitgrid/v2/BitGrid.Base.gd
+++ return-false/game/systems/puzzles/bitgrid/v2/BitGrid.Base.gd
@@ -10,10 +10,10 @@ var target_bits = []
 var active_bits = [] setget make_selection
 
 
-var styled = load("res://systems/puzzles/bitgrid/v2/stylebox_teal.tres")
-var styled2 = load("res://systems/puzzles/bitgrid/v2/stylebox_orange.tres")
-var unstyled = load("res://systems/puzzles/bitgrid/v2/stylebox_dark_teal.tres")
-var unstyled2 = load("res://systems/puzzles/bitgrid/v2/stylebox_dark_orange.tres")
+var styled = preload("res://systems/puzzles/bitgrid/v2/stylebox_teal.tres")
+var styled2 = preload("res://systems/puzzles/bitgrid/v2/stylebox_orange.tres")
+var unstyled = preload("res://systems/puzzles/bitgrid/v2/stylebox_dark_teal.tres")
+var unstyled2 = preload("res://systems/puzzles/bitgrid/v2/stylebox_dark_orange.tres")
 
 onready var current_grid = find_node('current')
 onready var target_grid = find_node('target')
@@ -26,26 +26,10 @@ onready var total = rows * cols
 func set_bit(index, group = current_bits, value=0):
 	assert(group != null and group.size() >= index)
 	group[index] = value
-	update_ui(group, true)
 	
-	# update UI here! #
-
-func flip_bit(index, group):
-	assert(group[index] != null)
-#	print('bit at index ', index, ' in group ', group, ' is ', group[index])
-	if int(group[index]) == 1:
-		set_bit(index, group, 0)
-		# group[index] = 0
-	else:
-		# group[index] = 1
-		set_bit(index, group, 1)
-	
-	update_ui(group, true)
-
-func get_bit(index, group = current_bits):
+func get_bit(index, group):
 	assert(group != null and group.size() >= index)
 	return group[index]
-
 
 # This gets overridden to create different game modes
 func _selection_from(index):
@@ -57,18 +41,17 @@ func make_selection(index):
 	update_ui(current_bits, true)
 
 
+# This assumes basic click-to-toggle for the whole selection at once;
+# support for fancier moves like "rotating" bits will come later.
+
 func make_move(index):
-	make_selection(index)
-	for i in range(active_bits.size()):
-		print(current_bits[i], " | ", current_bits[active_bits[i]])
-		if current_bits[active_bits[i]] == 1:
-			current_bits[active_bits[i]] = 0
-			return
-		current_bits[active_bits[i]] = 1
-		
-#	for i in range(active_bits.size()):
-#		flip_bit(active_bits[i], current_bits)
-	update_ui(current_bits, true)
+	for bit in active_bits:
+		if get_bit(bit, current_bits) == 1:
+			current_bits[bit] = 0
+		else:
+			current_bits[bit] = 1
+		update_ui(current_bits, true)
+
 
 # Note: this builds a half-0, half-1 array every time.
 # This can be changed by randomizing the target_bits assignments.
@@ -88,17 +71,18 @@ func build_ui():
 		var puzzle_btn = Button.new()
 		
 		target_grid.add_child(target_btn)
-		target_btn.set_text(str(get_bit(i, target_bits)))
+		target_btn.set_text(str(target_bits[i]))
 		target_btn.set_v_size_flags(3)
 		target_btn.set_h_size_flags(3)
 		target_btn.set_disabled(true)
 		
 		current_grid.add_child(puzzle_btn)
-		puzzle_btn.set_text(str(get_bit(i, current_bits)))
+		puzzle_btn.set_text(str(current_bits[i]))
 		puzzle_btn.set_v_size_flags(3)
 		puzzle_btn.set_h_size_flags(3)
 		puzzle_btn.connect('mouse_enter', self, 'make_selection', [i])
 		puzzle_btn.connect('pressed', self, 'make_move', [i])
+#		puzzle_btn.connect('released', self, 'update_ui', [current_bits, true])
 
 	update_ui(target_bits, true)
 	update_ui(current_bits, true)
@@ -107,7 +91,7 @@ func build_ui():
 
 func update_ui(group=current_bits, values_changed=false):
 	var grid_node = current_grid
-	if group == 'target_bits':
+	if group == target_bits:
 		grid_node = target_grid
 
 	var blocks = grid_node.get_children()
@@ -117,14 +101,33 @@ func update_ui(group=current_bits, values_changed=false):
 		
 		if values_changed:
 			block.set_text(str(group[j]))
-				#block.set_text(str(get_bit(j, group)))
+			block.update()
 		
-		block.add_style_override('normal', StyleBoxEmpty.new())
+		if block.is_disabled():
+			if block.get_text() == '1':
+				block.add_style_override('disabled', unstyled)
+			else:
+				block.add_style_override('disabled', unstyled2)
+		
+		if block.get_text() == '1':
+			block.add_style_override('normal', unstyled)
+			block.add_style_override('hover', styled)
+		else:
+			block.add_style_override('normal', unstyled2)
+			block.add_style_override('hover', styled2)
+		
 
 		if j in active_bits:
-
-			block.add_style_override('normal', styled)
-			block.add_style_override('hover', styled)
+			
+			if block.get_text() == '1':
+				block.add_style_override('normal', styled)
+				block.add_style_override('hover', styled)
+			
+			else:
+				block.add_style_override('normal', styled2)
+				block.add_style_override('hover', styled2)
+				
+			block.update()
 
 		# var block_val = get_bit(j, group)
 		# if j in active_bits:
