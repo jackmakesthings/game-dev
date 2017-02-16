8ba6abcd9e1f2dc99ef1c4ab19e4b7319ec8bd65
Jack Gold <hello@jackmakesthings.com>
2017-02-16 13:43:24 -0500

    more style refinements on #99, starting to actually look like the designs!

--- return-false/game/data/dialogue-blocks-test-json.gd
+++ return-false/game/data/dialogue-blocks-test-json.gd
@@ -2,77 +2,77 @@
   "blocks": [
     {
       "_name": "Abbott",
-      "_pic": "res://assets/temp/abbott.jpg",
+      "_pic": "res://assets/temp/abbott.png",
       "_text": "I'm telling you. Who's on first, What's on second, I Don't Know is on third--"
     },
     {
       "_name": "Costello",
-      "_pic": "res://assets/temp/costello.jpg",
+      "_pic": "res://assets/temp/costello.png",
       "_text": "You know the fellows' names?"
     },
     {
       "_name": "Abbott",
-      "_pic": "res://assets/temp/abbott.jpg",
+      "_pic": "res://assets/temp/abbott.png",
       "_text": "Yes."
     },
     {
       "_name": "Costello",
-      "_pic": "res://assets/temp/costello.jpg",
+      "_pic": "res://assets/temp/costello.png",
       "_text": "Well, then who's playing first?"
     },
     {
       "_name": "Abbott",
-      "_pic": "res://assets/temp/abbott.jpg",
+      "_pic": "res://assets/temp/abbott.png",
       "_text": "Yes."
     },
     {
       "_name": "Costello",
-      "_pic": "res://assets/temp/costello.jpg",
+      "_pic": "res://assets/temp/costello.png",
       "_text": "I mean the fellow's name on first base."
     },
     {
       "_name": "Abbott",
-      "_pic": "res://assets/temp/abbott.jpg",
+      "_pic": "res://assets/temp/abbott.png",
       "_text": "Who."
     },
     {
       "_name": "Costello",
-      "_pic": "res://assets/temp/costello.jpg",
+      "_pic": "res://assets/temp/costello.png",
       "_text": "The fellow playin' first base."
     },
     {
       "_name": "Abbott",
-      "_pic": "res://assets/temp/abbott.jpg",
+      "_pic": "res://assets/temp/abbott.png",
       "_text": "Who."
     },
     {
       "_name": "Costello",
-      "_pic": "res://assets/temp/costello.jpg",
+      "_pic": "res://assets/temp/costello.png",
       "_text": "The guy on first base."
     },
     {
       "_name": "Abbott",
-      "_pic": "res://assets/temp/abbott.jpg",
+      "_pic": "res://assets/temp/abbott.png",
       "_text": "Who is on first."
     },
     {
       "_name": "Costello",
-      "_pic": "res://assets/temp/costello.jpg",
+      "_pic": "res://assets/temp/costello.png",
       "_text": "Well, what are you askin' me for?"
     },
     {
       "_name": "Abbott",
-      "_pic": "res://assets/temp/abbott.jpg",
+      "_pic": "res://assets/temp/abbott.png",
       "_text": "I'm not asking you--I'm telling you. Who is on first."
     },
     {
       "_name": "Costello",
-      "_pic": "res://assets/temp/costello.jpg",
+      "_pic": "res://assets/temp/costello.png",
       "_text": "I'm asking you--who's on first?"
     },
     {
       "_name": "Abbott",
-      "_pic": "res://assets/temp/abbott.jpg",
+      "_pic": "res://assets/temp/abbott.png",
       "_text": "That's the man's name."
     }
   ]
